49eff0105b2b452acf280e233c6d154768c4bd86
JunKyle <claudine.manrique@gmail.com>
2025-02-09 15:06:32 +0100

    home page metadata

---
+++ src/app/headerNav/layout.tsx
@@ -0,0 +1,36 @@
+'use client';
+import { useState } from "react";
+import Link from 'next/link';
+
+export default function HeaderNav({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  const [openNav, setOpenNav] = useState(false);
+  return (
+    <>
+      <header className="Header">
+        <Link className="Header__link" href="/">
+          <div className="Header__image"></div>
+          <div className="gradient"></div>
+          <div className="Header__content">
+            <h1>Claudine Manrique</h1>
+            <span className="Header__subtitle">Ecrivain-e, artiste, développeur-se</span>
+          </div>
+          <span className="Header__nav" onClick={() => setOpenNav(true)}>&#9776;</span>
+        </Link>
+      </header>
+      <nav className={"Nav "+ (openNav ? 'show' : 'hidden')}>
+        <a className="Nav__close" onClick={() => setOpenNav(false)}>&times;</a>
+        <ul className="Nav__items">
+          <li className="Nav__item"><Link onClick={() => setOpenNav(false)} href="/qui-suis-je">qui suis-je</Link></li>
+          <li className="Nav__item"><Link onClick={() => setOpenNav(false)} href="/blog">blog</Link></li>
+          <li className="Nav__item"><Link onClick={() => setOpenNav(false)} href="/portefolio">portefolio développeuse</Link></li>
+          <li className="Nav__item"><Link onClick={() => setOpenNav(false)} href="/publications">publications</Link></li>
+          <li className="Nav__item"><Link onClick={() => setOpenNav(false)} href="/contact">contact</Link></li>
+        </ul>
+      </nav>
+      </>
+  );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,11 @@
-"use client"
 import philosopher from 'next/font/local';
+import type { Metadata } from 'next'
 import "./globals.css";
 import "./page.scss";
-import { useState } from "react";
 import Link from 'next/link';
+import dynamic from 'next/dynamic';
+
+const HeaderNavComponent = dynamic(() => import('./headerNav/layout'));
 
 const localFont = philosopher({
   src: [
@@ -18,37 +20,24 @@ const localFont = philosopher({
   ],
   variable: '--font-philosopher'
 })
+ 
+export const metadata: Metadata = {
+  title: 'Claudine Manrique',
+  description: 'mon site',
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const [openNav, setOpenNav] = useState(false);
   return (
-    <html lang="en">
-      <body className={`${localFont.variable} font-sans`}>
-        <header className="Header">
-          <Link className="Header__link" href="/">
-            <div className="Header__image"></div>
-            <div className="gradient"></div>
-            <div className="Header__content">
-              <h1>Claudine Manrique</h1>
-              <span className="Header__subtitle">Ecrivain-e, artiste, développeur-se</span>
-            </div>
-            <span className="Header__nav" onClick={() => setOpenNav(true)}>&#9776;</span>
-          </Link>
-        </header>
-        <nav className={"Nav "+ (openNav ? 'show' : 'hidden')}>
-          <a className="Nav__close" onClick={() => setOpenNav(false)}>&times;</a>
-          <ul className="Nav__items">
-            <li className="Nav__item"><Link onClick={() => setOpenNav(false)} href="/qui-suis-je">qui suis-je</Link></li>
-            <li className="Nav__item"><Link onClick={() => setOpenNav(false)} href="/blog">blog</Link></li>
-            <li className="Nav__item"><Link onClick={() => setOpenNav(false)} href="/portefolio">portefolio développeuse</Link></li>
-            <li className="Nav__item"><Link onClick={() => setOpenNav(false)} href="/publications">publications</Link></li>
-            <li className="Nav__item"><Link onClick={() => setOpenNav(false)} href="/contact">contact</Link></li>
-          </ul>
-        </nav>
+    <html lang="fr">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </head>
+      <body className={`${localFont.variable} font-sans`}>        
+        <HeaderNavComponent  />
         {children}
         <footer className="Footer">
           <ul className="Footer__items">
