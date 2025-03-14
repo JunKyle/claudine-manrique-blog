9d25fb7ac381a5865433835673742616c108fad9
JunKyle <claudine.manrique@gmail.com>
2025-03-14 14:53:52 +0100

    fix last blog post in home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -32,8 +32,8 @@ export default function Home() {
           <div className="Home__paragraph Home__paragraph--column">
             <h2 className="Home__title">Dernier article de blog</h2>
             <div className="Home__blog">
-              <Link className="Home__blogLink" href={`${data.posts[data.posts.length - 1].url}`}></Link>
-              <BlogComponent params={{slug: data.posts.length - 1}} />
+              <Link className="Home__blogLink" href={`${data.posts.findLast((post) =>post.published).url}`}></Link>
+              <BlogComponent params={{slug: data.posts.findLast((post) =>post.published).id}} />
             </div>
           </div>
         </section>
